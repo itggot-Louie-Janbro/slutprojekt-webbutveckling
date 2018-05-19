b574b4c8a9b36f31080edc8d72a684366ea12518
Louie Janbro <louie.janbro@itggot.se>
2018-05-19 18:19:12 +0200

    fixade javaskript o sånt ill hemsidan

---
+++ Uzbekistan_tourism/site/js/layout_searchbar.js
@@ -0,0 +1,7 @@
+function lay_searchbar_appear() {
+    var e = document.getElementsByClassName("lay_searchbar_input")[0];
+        e.style.width = "180px";
+        e.style.height = "20px";
+        e.style.visibility = "visible";
+        e.style.color = "red";
+}
