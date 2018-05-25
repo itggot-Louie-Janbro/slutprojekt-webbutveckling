e124d0c09182d9c98e7e67435ffa1e2a96abd85e
Louie Janbro <louie.janbro@itggot.se>
2018-05-25 09:53:30 +0200

    Har nu lagt till innehåll i mina desktop menyer

--- Uzbekistan_tourism/site/js/lay_hotels_menu.js
+++ Uzbekistan_tourism/site/js/lay_hotels_menu.js
@@ -1,7 +1,8 @@
 function lay_hotels_appear() {
     var e = document.getElementsByClassName("lay_menu_h")[0];
-        e.style.height = "500px";
+        e.style.height = "350px";
         e.style.visibility = "visible";
+        e.style.border = "1px solid red";
 }
 
 function lay_hotels_disappear() {
--- Uzbekistan_tourism/site/js/lay_tours_menu.js
+++ Uzbekistan_tourism/site/js/lay_tours_menu.js
@@ -1,7 +1,8 @@
 function lay_tours_appear() {
     var e = document.getElementsByClassName("lay_menu_t")[0];
-        e.style.height = "500px";
+        e.style.height = "350px";
         e.style.visibility = "visible";
+        e.style.border = "1px solid red";
 }
 
 function lay_tours_disappear() {
